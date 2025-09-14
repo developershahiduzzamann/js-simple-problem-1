acbf8c631dee520aa597ce69892df46dc26fdd28
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-09-14 19:26:13 +0600

    add a new line

--- variousTwo.js
+++ variousTwo.js
@@ -7,5 +7,19 @@ function evenSize(str){
         console.log('odd size');
     }
 }
-evenSize('dhaka')
-evenSize('faka')
+// evenSize('dhaka')
+// evenSize('faka')
+
+function dubleOrTriple(number, doDuble){
+    if(doDuble === true){
+          const result = number * 2;
+          return result;
+    }
+    else{
+        const result = number * 3;
+        return result
+    }
+      
+}
+console.log(dubleOrTriple(5, true));
+console.log(dubleOrTriple(5, false));
