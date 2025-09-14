448c398671e20a334023e0c2a1610ae02e01a022
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-09-14 18:59:24 +0600

    first commit

---
+++ variousTwo.js
@@ -0,0 +1,11 @@
+function evenSize(str){
+    const size = str.length;
+    if(size %2===0){
+        console.log('even size');
+    }
+    else{
+        console.log('odd size');
+    }
+}
+evenSize('dhaka')
+evenSize('faka')
