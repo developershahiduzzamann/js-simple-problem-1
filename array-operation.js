e92e16b64355ee1afb4cf3d7b17e4254c7d78884
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-09-16 20:20:05 +0600

    addad the sum of number

--- array-operation.js
+++ array-operation.js
@@ -1,7 +1,10 @@
 function sumOfNumber(numbers){
+    let sum = 0;
     for(const number of numbers){
         console.log(number);
+        sum = sum + number;
     }
+    return sum;
 }
 const nums = [12, 33, 45, 55, 5, 67];
 const sum = sumOfNumber(nums);
