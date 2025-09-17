22cbdb1c4966a281538595fd029689522c9f5508
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-09-17 19:36:01 +0600

    add the new line

--- even-only.js
+++ even-only.js
@@ -1,6 +1,8 @@
 function evenNumbersOnly(numbers){
     for(const number of numbers){
-        console.log(number);
+        if(number %2 === 0){
+            console.log(number);
+        }
     }
 }
 const numbers = [12, 45, 56, 67, 77, 65];
