46f5757ad3d362a858c928102a62cf32890dc97b
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-09-17 20:11:33 +0600

    add the even number array

--- even-only.js
+++ even-only.js
@@ -1,9 +1,13 @@
 function evenNumbersOnly(numbers){
+    const evens =[];
     for(const number of numbers){
         if(number %2 === 0){
-            console.log(number);
+            // console.log(number);
+            evens.push(number);
         }
     }
+    return evens;
 }
 const numbers = [12, 45, 56, 67, 77, 65];
-evenNumbersOnly(numbers);
+const evens = evenNumbersOnly(numbers);
+console.log(evens);
