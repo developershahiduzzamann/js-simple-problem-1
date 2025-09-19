01a859268190a898651914b6297c60e4057deb36
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-09-19 20:18:40 +0600

    add the sum of number

--- even-only.js
+++ even-only.js
@@ -10,3 +10,17 @@ function evenNumbersOnly(numbers){
 const numbers = [12, 45, 56, 67, 77, 65];
 const evens = evenNumbersOnly(numbers);
 console.log('even number are',evens);
+
+
+function sumOfevenNumbers(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        if(number %2 === 0){
+            console.log(number);
+            sum = sum + number;
+        }
+    }
+    return sum;
+}
+const sum = sumOfevenNumbers(numbers);
+console.log('sum of even numbers',sum);
