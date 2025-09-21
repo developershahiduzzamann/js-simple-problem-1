16230e8f8fd603e6c06b1f03202e1b336c2178a8
developershahiduzzaman <sabbir.mirpur58@gmail.com>
2025-09-21 22:02:39 +0600

    addad the new line

--- convart.js
+++ convart.js
@@ -3,4 +3,16 @@ function confeet(inch){
     return feet;
 }
 const sabbirHeight = confeet(75);
-console.log(sabbirHeight);
+// console.log(sabbirHeight);
+
+
+function inchToFeet2(inch){
+    const feetFraction = inch / 12;
+    const feetNumber = parseInt(feetFraction);
+    const inchRemaining = inch % 12;
+    const result = feetNumber + 'ft ' + inchRemaining + 'inch.';
+    return result
+
+}
+const sabbirHeight2 = inchToFeet2(75);
+console.log(sabbirHeight2);
